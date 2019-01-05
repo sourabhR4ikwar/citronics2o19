504f98d6db25167c6b312295a77e8fe072f9ce87
sourabhR4ikwar <sourabh.d.raikwar@gmail.com>
2019-01-06 01:58:44 +0530

    Minor changes to Javascript

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
 
 
-function openNav(){
-  document.getElementById("sideNav").style.width = "100%";
-}
-
-function closeNav(){
-  document.getElementById("sideNav").style.width = "0";
-}
-
-function dropMenu(){
-  document.getElementById("dropDown").classList.toggle("show");
-}
 
 var scrollobject = {};
 window.addEventListener('scroll',function(){

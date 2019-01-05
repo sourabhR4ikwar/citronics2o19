f07e20231f85d0f640f70eb1e52f9e57ae64b19e
sourabhR4ikwar <sourabh.d.raikwar@gmail.com>
2019-01-06 02:00:49 +0530

    Empty Main.js

--- js/Nav.js
+++ js/Nav.js
@@ -9,3 +9,21 @@ function closeNav(){
 function dropMenu(){
   document.getElementById("dropDown").classList.toggle("show");
 }
+var scrollobject = {};
+window.addEventListener('scroll',function(){
+
+    scrollobject = {
+        x: window.pageXOffset,
+        y: window.pageYOffset
+    };
+
+    if(scrollobject.y > 0)
+    {
+        document.getElementById('mainNav').classList.remove("topTouch");
+    }
+    else
+    {
+         document.getElementById('mainNav').classList.add("topTouch");
+    }
+
+});

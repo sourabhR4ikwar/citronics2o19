6e784ae84d19f6c9b7b747845ba99af65ccaf9fa
sourabhR4ikwar <sourabh.d.raikwar@gmail.com>
2019-01-06 01:43:27 +0530

    fixing Design Issuse and Completed Animation.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 function openNav(){
   document.getElementById("sideNav").style.width = "100%";
 }
@@ -9,3 +11,22 @@ function closeNav(){
 function dropMenu(){
   document.getElementById("dropDown").classList.toggle("show");
 }
+
+var scrollobject = {};
+window.addEventListener('scroll',function(){
+
+    scrollobject = {
+        x: window.pageXOffset,
+        y: window.pageYOffset
+    };
+
+    if(scrollobject.y > 0)
+    {
+        document.getElementById('mainNav').classList.remove("topTouch");
+    }
+    else
+    {
+         document.getElementById('mainNav').classList.add("topTouch");
+    }
+
+});

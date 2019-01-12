b9cccc7eb483ceb7d73b4ebdbac5f1b4004e40aa
sourabhR4ikwar <sourabh.d.raikwar@gmail.com>
2019-01-13 01:51:38 +0530

    Ready to be presented

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
+var eventPage = document.getElementById('eventPage');
 
+function openNav(){
+  document.getElementById("sideNav").style.width = "100%";
+}
+
+function closeNav(){
+  document.getElementById("sideNav").style.width = "0";
+}
+
+function dropMenu(){
+  document.getElementById("dropDown").classList.toggle("show");
+}
+
+function openEvent(){
+  eventPage.classList.remove("deactive");
+}
+function closeEvent(){
+  eventPage.classList.add("deactive");
+}
+
+var scrollobject = {};
+window.addEventListener('scroll',function(){
+
+    scrollobject = {
+        x: window.pageXOffset,
+        y: window.pageYOffset
+    };
+
+    if(scrollobject.y > 0)
+    {
+        document.getElementById('mainNav').classList.remove("topTouch");
+        document.getElementById('homeContent').classList.add("fadeAway");
+    }
+    else
+    {
+         document.getElementById('mainNav').classList.add("topTouch");
+         document.getElementById('homeContent').classList.remove("fadeAway");
+    }
+
+});
